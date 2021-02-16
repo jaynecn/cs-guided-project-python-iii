bd879d59c576d92d5f7762908938cd19c66243b0
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2021-02-16 02:39:58 +0000

    finished question 2

--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -10,5 +10,38 @@ Examples:
 - single_number([10]) -> 10
 """
 def single_number(nums):
+    
+    seen_numbers_dict = {}
     # Your code here
+    
+    # UPER
+    # search for the only unique number
+    # is a number unique?
+    # use count function
+    # loop over each items and compare it to the first one to see if it repeats
+    
+            # TIME COMPLEXITY IS O(N) * 0(N) = O(N**2)
+            # for data in nums:
+            #     if nums.count(data) == 1:
+            #         return data
+        
+    # NOW TRY TO REDUCE TIME COMPLEXITY
+    for data in nums:
+        if data in seen_numbers_dict:
+            seen_numbers_dict[data] += 1
+            
+        else:
+            # Otherwise if first time seeing the number
+            seen_numbers_dict[data] = 1
+            
+    print(seen_numbers_dict)
+    # find the number thats only seen once
+    for key, value in seen_numbers_dict.items():
+        if value == 1:
+            return key
+        
 
+
+print(single_number([3,3,2]))
+print(single_number([5,2,3,2,3]))
+print(single_number([10]))
